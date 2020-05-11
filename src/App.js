638afeef0e4fc9ad81b053556c26048cff1e8f9f
Neal Satitsumpun <satitsumpun@wisc.edu>
2020-05-11 01:01:42 -0500

    Add AuthContext.Provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { useReducer } from 'react'
 import {
   BrowserRouter,
   Switch,
   Route,
   Link,
-  withRouter
-} from "react-router-dom";
-import Home from './Home'
-import History from './History/History'
+  withRouter,
+  Redirect
+} from "react-router-dom"
 
-class App extends Component {
-  render() {
-    return (
+import './App.css';
+import Home from './components/Search/Home'
+import History from './components/History/History'
+import Login from './components/Auth/Login'
+// import Register from './Auth/Register'
+
+const initialAuthState = {
+  token: null,
+  userId: null,
+  isAuthenticated: false
+}
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { token: action.payload.token, userId: action.payload.userId, isAuthenticated: true }
+    case 'LOGOUT':
+      return { token: null, userID: null, isAuthenticated: false}
+    default:
+      throw new Error('Auth reducer error')
+  }
+}
+
+export const AuthContext = React.createContext()
+
+const App = () => {
+  const [authState, authDispatch] = useReducer(authReducer, initialAuthState)
+  //TODO: handhle expired token
+  const { token, userId, isAuthenticated } = authState
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />
+  )
+  // TODO: handle logout
+  return (
+    <AuthContext.Provider value={{ state: authState, dispatch: authDispatch }}>
       <BrowserRouter>
         <div>
           <nav>
@@ -21,22 +56,23 @@ class App extends Component {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/history">History</Link>
+                { isAuthenticated ? <Link to="/history">History</Link> : <Link to="/login">Login</Link> } 
               </li>
             </ul>
           </nav>
-  
-          {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route> and
               renders the first one that matches the current URL. */}
           <Switch>
-            <Route path="/history" component={History} />
+            <PrivateRoute path="/history" component={History} />
+            <Route path="/login" component={Login} />
+            {/* <Route path="/register" component={Register} /> */}
             <Route path="/search" component={withRouter(Home)} />
             <Route path="/" component={withRouter(Home)} />
           </Switch>
         </div>
-      </BrowserRouter>
-    );    
-  }
+      </BrowserRouter>      
+    </AuthContext.Provider>
+  )   
 }
 
 export default App;
